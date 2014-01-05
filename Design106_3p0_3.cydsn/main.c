9d3ec970c92f6295b45895aa461ec6bc908ca7e1
noritan <github@noritan.org>
2014-01-05 13:06:48 +0900

    Implemented a single channel Power Monitor

--- Design106_3p0_3.cydsn/main.c
+++ Design106_3p0_3.cydsn/main.c
@@ -13,12 +13,26 @@
 
 int main()
 {
+    uint16 c1;
+    uint16 v1;
+    
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    LCD_Start();
 
-    /* CyGlobalIntEnable; */ /* Uncomment this line to enable global interrupts. */
+    CyGlobalIntEnable; /* Uncomment this line to enable global interrupts. */
+    PowerMonitor_Start();
     for(;;)
     {
         /* Place your application code here. */
+        c1 = PowerMonitor_GetConverterCurrent(1) * 1000;
+        v1 = PowerMonitor_GetConverterVoltage(1);
+        
+        LCD_Position(0, 0);
+        LCD_PrintDecUint16(c1);
+        LCD_PrintString("  ");
+        LCD_Position(1, 0);
+        LCD_PrintDecUint16(v1);
+        LCD_PrintString("  ");
     }
 }
 
